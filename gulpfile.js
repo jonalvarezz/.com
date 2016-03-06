59849f1bea9980f180a88e7a06c46576426e1764
Jonathan Alvarez G <j@jonalvarezz.com>
2016-03-05 20:04:09 -0500

    Add Gulp and Stylus support

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+var nib = require('nib');
+var through = require('through');
+var isDev = process.argv.indexOf('watch') !== -1;
+
+var baseDir = './contents/';
+
+// [DEV] Process CSS files and reload the web browser
+gulp.task('styles:build', function() {
+  return gulp.src(baseDir + 'stylus/main.styl')
+    .pipe(! isDev ? through() : $.plumber())
+    .pipe($.sourcemaps.init())
+      .pipe($.stylus({
+        'use': nib()
+      }))
+      .pipe(! isDev ? $.minifyCss() : through())
+    .pipe($.sourcemaps.write())
+    .pipe(gulp.dest(baseDir + 'css/'));
+});
+
+
+gulp.task('watch', function() {
+  gulp.watch( [baseDir + 'stylus/**/*.styl'], ['styles:build'] )
+});
+
+gulp.task('default', [ 'styles:build']);
