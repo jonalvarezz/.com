6f440b14756006909294c4126b1aa7a952697829
Jonathan Alvarez G <j@jonalvarezz.com>
2019-11-30 02:04:18 +0100

    Add build task

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,52 @@
-'use strict';
+"use strict";
 
-var gulp = require('gulp');
-var $ = require('gulp-load-plugins')();
-var nib = require('nib');
-var through = require('through');
-var isDev = process.argv.indexOf('watch') !== -1;
-var wintersmith = require('run-wintersmith');
-var runSequence = require('run-sequence');
+var gulp = require("gulp");
+var $ = require("gulp-load-plugins")();
+var nib = require("nib");
+var through = require("through");
+var isDev = process.argv.indexOf("watch") !== -1;
+var wintersmith = require("run-wintersmith");
+var runSequence = require("run-sequence");
 
 var PORT = 8080;
-var baseDir = './contents/';
+var baseDir = "./contents/";
 wintersmith.settings.port = PORT;
 
 // [DEV] Process CSS files and reload the web browser
-gulp.task('styles:build', function() {
-  return gulp.src(baseDir + 'stylus/main.styl')
-    .pipe(! isDev ? through() : $.plumber())
+gulp.task("styles:build", function() {
+  return gulp
+    .src(baseDir + "stylus/main.styl")
+    .pipe(!isDev ? through() : $.plumber())
     .pipe($.sourcemaps.init())
-      .pipe($.stylus({
-        'use': nib()
-      }))
-      .pipe(! isDev ? $.minifyCss() : through())
+    .pipe(
+      $.stylus({
+        use: nib()
+      })
+    )
+    .pipe(!isDev ? $.minifyCss() : through())
     .pipe($.sourcemaps.write())
-    .pipe(gulp.dest(baseDir + 'css/'));
+    .pipe(gulp.dest(baseDir + "css/"));
 });
 
-gulp.task('publish', function () {
-  return gulp.src('./build/**/*').pipe($.ghPages());
-})
-
-gulp.task('deploy', ['styles:build'], function() {
-  wintersmith.build(function () {
-    runSequence('publish')
-  })
+gulp.task("publish", function() {
+  return gulp.src("./build/**/*").pipe($.ghPages());
 });
 
+gulp.task("deploy", ["styles:build"], function() {
+  wintersmith.build(function() {
+    runSequence("publish");
+  });
+});
 
-gulp.task('watch', ['default'], function() {
+gulp.task("build", ["styles:build"], function() {
+  wintersmith.build(function() {
+    wintersmith.preview();
+  });
+});
+
+gulp.task("watch", ["default"], function() {
   wintersmith.preview();
-  gulp.watch( [baseDir + 'stylus/**/*.styl'], ['styles:build'] )
+  gulp.watch([baseDir + "stylus/**/*.styl"], ["styles:build"]);
 });
 
-gulp.task('default', [ 'styles:build']);
+gulp.task("default", ["styles:build"]);
